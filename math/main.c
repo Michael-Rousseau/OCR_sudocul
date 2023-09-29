0429d931d234fc19ba4120fa4360b9df604474e8
Arthur de Rugy <arthur.goullet-de-rugy@epita.fr>
2023-09-29 02:10:51 +0200

    add tests for sigmoid, prime_sigmoid and matd_mul

--- math/main.c
+++ math/main.c
@@ -1,22 +1,84 @@
 #include "matrix.h"
+#include "sigmoid.h"
+
 #include <stddef.h>
+#include <stdio.h>
+#include <assert.h>
+
+int test_matrixd() {
+    printf("Testing matrix.c...\n");
+    size_t rows_a = 2, cols_a = 3, cols_b = 2;
+
+    double *a = matrixd(rows_a, cols_a); // 2*3 Matrix
+    double *b = matrixd(cols_a, cols_b); // 3*2 Matrix
+
+    // Fill Matrix a
+    matd_set(a, rows_a, cols_a, 0, 0, 1.0);
+    matd_set(a, rows_a, cols_a, 1, 0, 2.0);
+    matd_set(a, rows_a, cols_a, 2, 0, 3.0);
+    matd_set(a, rows_a, cols_a, 0, 1, 4.0);
+    matd_set(a, rows_a, cols_a, 1, 1, 5.0);
+    matd_set(a, rows_a, cols_a, 2, 1, 6.0);
+
+    // Fill Matrix b
+    matd_set(b, cols_a, cols_b, 0, 0, 7.0);
+    matd_set(b, cols_a, cols_b, 1, 0, 8.0);
+    matd_set(b, cols_a, cols_b, 0, 1, 9.0);
+    matd_set(b, cols_a, cols_b, 1, 1, 10.0);
+    matd_set(b, cols_a, cols_b, 0, 2, 11.0);
+    matd_set(b, cols_a, cols_b, 1, 2, 12.0);
+
+    // Multiply a and b
+    double *result = matd_mul(a, b, rows_a, cols_a, cols_b);
+
+    // Check the result
+    assert(matd_get(result, rows_a, cols_b, 0, 0) == 58.0);
+    assert(matd_get(result, rows_a, cols_b, 1, 0) == 64.0);
+    assert(matd_get(result, rows_a, cols_b, 0, 1) == 139.0);
+    assert(matd_get(result, rows_a, cols_b, 1, 1) == 154.0);
+
+    // Free the matrices
+    matd_free(a);
+    matd_free(b);
+    matd_free(result);
+
+    printf("All tests passed!\n");
+    return 0;
+}
+
+void test_sigmoid() {
+    double value = 1;
+    printf("Sigmoid of %6f:\n", value);
+    printf("\tExpected: %6f\n", 0.731059);
+    printf("\tActual  : %6f\n", sigmoid(value));
+
+
+    value = 0;
+    printf("Sigmoid of %6f:\n", value);
+    printf("\tExpected: %6f\n", 0.5);
+    printf("\tActual  : %6f\n", sigmoid(value));
+
+    printf("\n");
+}
+
+void test_prime_sigmoid() {
+    double value = 1;
+    printf("Prime sigmoid of %6f:\n", value);
+    printf("\tExpected: %6f\n", 0.196612);
+    printf("\tActual  : %6f\n", prime_sigmoid(value));
+
+
+    value = 0;
+    printf("Prime sigmoid of %6f:\n", value);
+    printf("\tExpected: %6f\n", 0.25);
+    printf("\tActual  : %6f\n", prime_sigmoid(value));
+
+    printf("\n");
+}
 
 int main() {
-    size_t ROWS = 2;
-    size_t COLS = 3;
+    test_sigmoid();
+    test_prime_sigmoid();
 
-    double *mat = matrixd(ROWS, COLS);
-    double *mat2 = matrixd(COLS, ROWS);
-
-    matd_set(mat, ROWS, COLS, 2, 1, 2);
-
-    double *r = matd_mul(mat, mat2, ROWS, COLS, ROWS);
-
-    matd_print(mat, ROWS, COLS);
-    matd_print(mat2, COLS, ROWS);
-    matd_print(r, ROWS, ROWS);
-
-    matd_free(mat);
-    matd_free(mat2);
-    matd_free(r);
+    return test_matrixd();
 }
