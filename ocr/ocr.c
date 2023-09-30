8e4c23aac289ba9caba897cf4c75a0c7c225ca8b
Arthur de Rugy <arthur.goullet-de-rugy@epita.fr>
2023-09-30 23:34:28 +0200

    fast_forward + read_output

--- ocr/ocr.c
+++ ocr/ocr.c
@@ -15,9 +15,26 @@ double prime_sigmoid(double x) {
 }
 
 void feed_forward(network *n, double *inputs) {
+    for (size_t layer = 1; layer < n->len; layer++) {
+        for (size_t i = 0; i < n->layers[layer]; i++) {
+            double sum = 0;
 
+            for (size_t j = 0; j < n->layers[layer - 1]; j++)
+                sum += n->values[i - 1][j];
+
+            n->values[layer][i] = sigmoid(sum + n->biases[layer - 1][i]);
+        }
+    }
 }
 
-double read_output(network *n) {
+size_t read_output(network *n) {
+    size_t last = n->len - 1;
 
+    size_t max = 0;
+    for (size_t i = 1; i < n->layers[last]; i++) {
+        if (n->values[last][i] > n->values[last][max])
+            max = i;
+    }
+
+    return max;
 }
--- ocr/ocr.h
+++ ocr/ocr.h
@@ -1,4 +1,4 @@
 #pragma once
 
 void feed_forward(network *n, double *inputs);
-double read_output(network *n);
+size_t read_output(network *n);
