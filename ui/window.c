b65445fb1c6594d9380bdbdb401a7f5424cf1dda
Roman Morliere <roman.morliere@epita.fr>
2023-11-27 16:34:00 +0100

    updating window.c

--- ui/window.c
+++ ui/window.c
@@ -1,9 +1,137 @@
 #include <gtk/gtk.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 
 GtkWidget *image;
 int is_loaded = 0;
 char *file_path;
+
+const char *image_paths[] = {
+    "./data/grayscale.png",
+    "./data/inverse.png",
+    "./data/contrast.png",
+    "./data/reducenoise.png",
+    "./data/erosion.png",
+    "./data/dilation.png",
+    "./data/canny.png"
+};
+
+
+int start_matrix[9][9] = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9},
+    {1, 2, 3, 4, 5, 6, 7, 8, 9}};
+
+
+int end_matrix[9][9] = {{9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1},
+    {9, 8, 7, 6, 5, 4, 3, 2, 1}};
+
+
+typedef struct {
+    GtkWidget *mainGrid;
+    GtkWidget *sudokuGrid;
+} GridData;
+
+
+
+static gboolean display_next_image(gpointer user_data) {
+    static int current_image = 0;
+    const int total_images = sizeof(image_paths) / sizeof(image_paths[0]);
+
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_at_scale(image_paths[current_image], 600, 600, TRUE, NULL);
+    if (pixbuf) {
+        gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+        g_object_unref(pixbuf);
+    }
+
+    current_image = (current_image + 1) % total_images;
+    return (current_image != 0);
+}
+
+
+static void on_epic_button_clicked(GtkWidget *widget, gpointer user_data) {
+    if (!is_loaded){
+        g_print("Image has not been loaded\n");
+        return;
+    }
+    g_timeout_add_seconds(1, display_next_image, NULL);
+}
+
+
+
+static void get_the_grid(GtkWidget *widget, gpointer user_data) {
+    if ( !is_loaded ){
+        g_print("Image has not been loaded\n");
+        return;
+    }
+
+    if(GTK_IS_IMAGE(image)){
+        GridData *data = (GridData *)user_data;
+
+        g_print("The image is a GTK_image\n");
+        gtk_image_clear(image);
+        is_loaded = 0;
+
+
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                char buffer[3];
+                snprintf(buffer, sizeof(buffer), "%d", start_matrix[row][col]);
+
+
+                GtkWidget *number_label = gtk_button_new_with_label(buffer);
+                gtk_grid_attach(GTK_GRID(data->sudokuGrid), number_label, col, row, 1, 1);
+            }
+        }
+
+        gtk_grid_attach(GTK_GRID(data->mainGrid), data->sudokuGrid, 0, 2, 1, 1);
+        gtk_widget_show_all(data->mainGrid);
+    }
+    else {
+        g_print("Not a gtk image\n");
+    }
+}
+
+
+
+
+
+static void solve_image(GtkWidget *widget, gpointer user_data) {
+    GridData *data = (GridData *)user_data;
+
+    if(!gtk_grid_get_child_at(GTK_GRID(data->sudokuGrid), 0, 0)){
+        g_print("Grid has not been loaded !\n");
+    }
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            GtkWidget *child = gtk_grid_get_child_at(GTK_GRID(data->sudokuGrid), col, row);
+
+            if (GTK_IS_BUTTON(child)) {
+                char buffer[3];
+                snprintf(buffer, sizeof(buffer), "%d", end_matrix[row][col]);
+                gtk_button_set_label(GTK_BUTTON(child), buffer);
+            }
+        }
+    }
+    gtk_widget_show_all(data->mainGrid);
+}
+
 
 
 static void clicked_image(GtkWidget *widget, gpointer data) {
@@ -12,8 +140,7 @@ static void clicked_image(GtkWidget *widget, gpointer data) {
         return;
     }
 
-
-    char *base_path = "/mnt/c/Users/roman/OneDrive/Desktop/EPITA/SPE/SUDOCUL/OCR_sudocul/ui/data";
+    char *base_path = "./data";
     int length = strlen(base_path) + strlen(data) + 2;  // +2 for the null terminator and a slash
 
     // Allocate memory
@@ -94,6 +221,7 @@ static void activate(GtkApplication* app, gpointer user_data) {
     GtkWidget *grid;
     GtkWidget *button_grid;
 
+    GtkWidget *epic_button;
 
 
     grid = gtk_grid_new();
@@ -102,7 +230,7 @@ static void activate(GtkApplication* app, gpointer user_data) {
     gtk_grid_attach(GTK_GRID(grid), button_grid, 1, 2, 1, 1);
 
 
-    GtkCssProvider *css_provider = gtk_css_provider_new();
+   GtkCssProvider *css_provider = gtk_css_provider_new();
     gtk_css_provider_load_from_path(css_provider, "style.css", NULL);
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
             GTK_STYLE_PROVIDER(css_provider),
@@ -117,73 +245,81 @@ static void activate(GtkApplication* app, gpointer user_data) {
 
     grayscale = gtk_button_new_with_label("GRAYSCALE");
     g_signal_connect(grayscale, "clicked", G_CALLBACK(clicked_image), "grayscale.png");
-    gtk_container_add(GTK_CONTAINER(grayscale), grid);
     gtk_grid_attach(GTK_GRID(button_grid), grayscale, 0, 0, 1, 1);
 
 
     threshold = gtk_button_new_with_label("THRESHOLD");
-    g_signal_connect(threshold, "clicked", G_CALLBACK(clicked_image), "canny.png");
-    gtk_container_add(GTK_CONTAINER(threshold), grid);
+    g_signal_connect(threshold, "clicked", G_CALLBACK(clicked_image), "inverse.png");
     gtk_grid_attach(GTK_GRID(button_grid), threshold, 0, 1, 1, 1);
 
 
     contrast = gtk_button_new_with_label("CONTRAT");
     g_signal_connect(contrast, "clicked", G_CALLBACK(clicked_image), "contrast.png");
-    gtk_container_add(GTK_CONTAINER(contrast), grid);
     gtk_grid_attach(GTK_GRID(button_grid), contrast, 0, 2, 1, 1);
 
 
     reducenoise = gtk_button_new_with_label("REDUCENOISE");
     g_signal_connect(reducenoise, "clicked", G_CALLBACK(clicked_image), "reducenoise.png");
-    gtk_container_add(GTK_CONTAINER(reducenoise), grid);
     gtk_grid_attach(GTK_GRID(button_grid), reducenoise, 0, 3, 1, 1);
 
 
     erosion  = gtk_button_new_with_label("EROSION");
     g_signal_connect(erosion, "clicked", G_CALLBACK(clicked_image), "erosion.png");
-    gtk_container_add(GTK_CONTAINER(erosion), grid);
     gtk_grid_attach(GTK_GRID(button_grid), erosion, 0, 4, 1, 1);
 
 
     dilation = gtk_button_new_with_label("DILATION ");
     g_signal_connect(dilation, "clicked", G_CALLBACK(clicked_image), "dilation.png");
-    gtk_container_add(GTK_CONTAINER(dilation), grid);
     gtk_grid_attach(GTK_GRID(button_grid), dilation, 0, 5, 1, 1);
 
     canny = gtk_button_new_with_label("CANNY");
     g_signal_connect(canny, "clicked", G_CALLBACK(clicked_image), "canny.png");
-    gtk_container_add(GTK_CONTAINER(canny), grid);
     gtk_grid_attach(GTK_GRID(button_grid), canny, 0, 6, 1, 1);
 
+    epic_button = gtk_button_new_with_label("EPIC BUTTON");
+    g_signal_connect(epic_button, "clicked", G_CALLBACK(on_epic_button_clicked), NULL);
+    gtk_grid_attach(GTK_GRID(button_grid), epic_button, 0, 9, 1, 1);
+
+    GtkWidget *solve, *get_grid;
+
+    GridData *data = malloc(sizeof(GridData));
+    data->mainGrid = grid;
+    data->sudokuGrid = gtk_grid_new();
 
 
-    GtkWidget *solve;
+    get_grid = gtk_button_new_with_label("GET GRID");
+    g_signal_connect(get_grid, "clicked", G_CALLBACK(get_the_grid), data);
+    gtk_grid_attach(GTK_GRID(button_grid), get_grid, 0, 7, 1, 1);
+
 
     solve = gtk_button_new_with_label("SOLVE");
-    g_signal_connect(solve, "clicked", G_CALLBACK(clicked_image), image);
-    gtk_container_add(GTK_CONTAINER(solve), grid);
-    gtk_grid_attach(GTK_GRID(button_grid), solve, 0, 7, 1, 1);
+    g_signal_connect(solve, "clicked", G_CALLBACK(solve_image), data);
+    gtk_grid_attach(GTK_GRID(button_grid), solve, 0, 8, 1, 1);
 
 
 
 
-
-
+   // Create a new file chooser button
     file_chooser = gtk_file_chooser_button_new("Select a PNG File", GTK_FILE_CHOOSER_ACTION_OPEN);
 
+    // Set up a filter for PNG files
     GtkFileFilter *filter = gtk_file_filter_new();
     gtk_file_filter_add_mime_type(filter, "image/png");
     gtk_file_chooser_set_filter(GTK_FILE_CHOOSER(file_chooser), filter);
 
+    // Create a new image widget
     image = gtk_image_new();
     gtk_grid_attach(GTK_GRID(grid), image, 0, 2, 1, 1);
 
+    // Connect the "file-set" signal of the file chooser to the on_file_chosen function
     g_signal_connect(file_chooser, "file-set", G_CALLBACK(on_file_chosen), image);
+
+    // Add the file chooser to the grid
     gtk_grid_attach(GTK_GRID(grid), file_chooser, 0, 1, 1, 1);
 
-
-
+    // Show all widgets in the window
     gtk_widget_show_all(window);
+
 }
 
 
@@ -202,4 +338,3 @@ int main(int argc, char **argv) {
 
     return status;
 }
-
