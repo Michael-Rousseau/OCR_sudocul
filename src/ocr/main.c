8a1747f0bce8f34efd0e5540ee234112fe56a520
Michael-Rousseau <michael.rousseau@epita.fr>
2023-12-04 21:51:12 +0100

    now working

--- src/ocr/main.c
+++ src/ocr/main.c
@@ -24,13 +24,13 @@ void train_batch(network *n, double speed, double **batch_inputs,
 
 void train(network *n, double speed, double **inputs, int **targets,
            size_t num_samples, size_t num_epochs, size_t batch_size) {
-    load_mnist();
+  load_mnist();
   for (size_t epoch = 0; epoch < num_epochs; ++epoch) {
     size_t correct_predictions = 0;
 
-    size_t index =0;
+    size_t index = 0;
     for (size_t start = 0; start < num_samples; start += batch_size) {
-      size_t end = start + batch_size;
+      size_t end = start +batch_size ;
       if (end > num_samples) {
         end = num_samples;
       }
@@ -41,14 +41,15 @@ void train(network *n, double speed, double **inputs, int **targets,
       train_batch(n, speed, batch_inputs, batch_targets, current_batch_size);
       for (size_t i = 0; i < current_batch_size; ++i) {
         feed_forward(n, batch_inputs[i]);
-        //printf("result: %zu\nexpected: %d\n", read_output(n), train_label[index]);
-        if((int)read_output(n) == train_label[index++])
+        if ((int)read_output(n) == train_label[index++])
           correct_predictions++;
         if ((start + i + 1) % 1000 == 0) {
           double accuracy_so_far =
-              (double)correct_predictions / (start + i + 1) * 100.0;
-          printf("Epoch %zu - Tests: %zu - Accuracy: %.2f%%\n Correct: %zu\n", epoch + 1,
-                 start + i + 1, accuracy_so_far, correct_predictions);
+              (double)correct_predictions / (10.0);
+          printf("Epoch %zu - Tests: %zu - Accuracy: %.2f%%\n Correct: %zu\n",
+                 epoch + 1, start + i + 1, accuracy_so_far,
+                 correct_predictions);
+          correct_predictions = 0;
         }
       }
     }
@@ -98,10 +99,15 @@ void load_and_train(network *n) {
     if (a == -1)
       err(1, "Error writing in tab");
 
-    double *current_image = load_image(filename);
-
+    double *current_image = train_image[i];
     for (size_t i = 0; i < 784; ++i)
-      current_image[i] /= 255.0;
+    {
+        if(current_image[i] > 0.5)
+            current_image[i] = 1;
+        else
+         current_image[i] = 0;
+    }
+
 
     if (current_image == NULL)
       errx(1, "Didn't get the image from load");
@@ -114,13 +120,12 @@ void load_and_train(network *n) {
   }
 
   size_t num_samples = NUM_IMAGES;
-  size_t num_epochs = 1;
-  size_t batch_size = 1;
+  size_t num_epochs = 10;
+  size_t batch_size = 10;
 
   train(n, LEARNING_RATE, inputs, targets, num_samples, num_epochs, batch_size);
 
   for (int i = 0; i < NUM_IMAGES; ++i) {
-    free(inputs[i]);
     free(targets[i]);
   }
   free(inputs);
@@ -134,10 +139,30 @@ void build_Images(int i) {
   }
 }
 
-void test_from_load()
-{
-    network* n = import_network("testnetwork");
+void test_from_load(int nb_test) {
+  network *n = import_network("testnetwork");
+  load_mnist();
+  int accuracy = 0;
+  for (int i = 0; i < nb_test; ++i) {
+    int expected = test_label[i];
 
+    double *inputs = test_image[i];
+    for (int j = 0; j < 784; ++j)
+    {
+        if(inputs[i] >0.5)
+            inputs[i] = 1;
+        else
+         inputs[i] = 0;
+    }
+    feed_forward(n, inputs);
+    if(read_output(n) == expected)
+    {
+        accuracy++;
+    }
+    printf("result: %zu\nexpected: %d\n", read_output(n), expected);
+  }
+  double final = (double)accuracy / (double)nb_test; 
+  printf("final : %f\n", final * 100);
 }
 int main() {
   size_t *layers = malloc(LAYER_COUNT * sizeof(size_t));
@@ -146,12 +171,14 @@ int main() {
   layers[2] = 10;
   network *n = xavier_init_network(layers, LAYER_COUNT);
 
-  //first: call build_images and then comment it
+  // first: call build_images and then comment it
 
-//build_Images(NUM_IMAGES);
-  load_and_train(n);
+  // build_Images(NUM_IMAGES);
+ load_and_train(n);
 
-    export_network(n, "testnetwork");
+   export_network(n, "testnetwork");
+  test_from_load(1000);
+
   free_network(n);
   return 0;
 }
--- src/ocr/ocr.h
+++ src/ocr/ocr.h
@@ -5,3 +5,8 @@ void back_prop(network *n, int *expected);
 void learn(network *n, double speed);
 size_t read_output(network *n);
 
+struct ThreadData {
+    network *n;
+    size_t start;
+    size_t end;
+};
